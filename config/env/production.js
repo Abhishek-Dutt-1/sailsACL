21e46b13581c734aae84f6d2d99e192dec9d47ef
Abhishek Dutt <abhishekdutt@outlook.com>
2015-02-08 19:19:23 +0530

    Setting up production

--- config/env/production.js
+++ config/env/production.js
@@ -20,12 +20,16 @@ module.exports = {
   // models: {
   //   connection: 'someMysqlServer'
   // },
+    models: {
+        migrate: 'safe'
+    },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
   // port: 80,
+  port: process.env.PORT || 80,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
